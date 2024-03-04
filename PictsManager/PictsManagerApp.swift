d5521642491e6134842d9ae10b3efa1bcffb0798
Charles Lamarque <charles.lamarque69@gmail.com>
2024-03-04 14:42:46 +0100

    Initial Commit

---
+++ PictsManager/PictsManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictsManagerApp.swift
+//  PictsManager
+//
+//  Created by Charles Lamarque on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct PictsManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
