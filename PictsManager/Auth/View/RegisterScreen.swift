8aa96c37006020fd2ed85895f054aebaa7ca9b8a
minhduc <ducbv1407@gmail.com>
2024-03-11 09:55:34 +0100

    init register screen

---
+++ PictsManager/Auth/View/RegisterScreen.swift
@@ -0,0 +1,57 @@
+//
+//  RegisterScreen.swift
+//  PictsManager
+//
+//  Created by Minh Duc on 11/03/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct RegisterScreen: View {
+    @ObservedObject var viewModel: AuthViewModel
+    @State private var username = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack {
+            Text("Register")
+                .font(.title)
+                .bold()
+            
+            TextField("Username", text: $username)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                let user = User(username: username, password: password)
+                viewModel.register(user: user)
+            }) {
+                Text("Register")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+        }
+        .padding()
+    }
+}
+
+struct RegisterScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = AuthViewModel()
+        RegisterScreen(viewModel: viewModel)
+    }
+}
