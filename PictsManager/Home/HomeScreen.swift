d3008be26d78034dcf27a512562bf9703d723fd1
minhduc <ducbv1407@gmail.com>
2024-03-16 11:21:17 +0100

    style: change login/register visual

--- PictsManager/Home/HomeScreen.swift
+++ PictsManager/Home/HomeScreen.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct HomeScreen: View {
     var body: some View {
-        NavigationView{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
