9de87db999dabd5612bfc3e7c98b7cb5620fb3e1
Charles Lamarque <charles.lamarque69@gmail.com>
2024-03-19 11:57:08 +0100

    Add myAlbumsView with two rows and related albums

---
+++ PictsManager/Albums/AlbumPreview.swift
@@ -0,0 +1,30 @@
+//
+//  AlbumPreview.swift
+//  PictsManager
+//
+//  Created by Charles Lamarque on 18/03/2024.
+//
+
+import SwiftUI
+
+struct AlbumPreview: View {
+  var album: Album
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      album.pictures[0]
+        .renderingMode(.original)
+        .resizable()
+        .frame(width: 155, height: 155)
+        .cornerRadius(3)
+      Text(album.name)
+        .padding(.bottom, -8)
+      Text(String(album.pictureNames.count))
+        .foregroundStyle(Color.gray)
+    }
+  }
+}
+
+#Preview {
+  AlbumPreview(album: Album(id: 12, name: "Vacances", pictureNames: ["twinlake", "02", "03", "04", "05", "06", "07", "07","07","07","07","07","07","07","07","07","07","07","07"]))
+}
---
+++ PictsManager/Albums/FavoritesAlbumPreview.swift
@@ -0,0 +1,35 @@
+//
+//  FavoritesAlbumPreview.swift
+//  PictsManager
+//
+//  Created by Charles Lamarque on 19/03/2024.
+//
+
+import SwiftUI
+
+struct FavoritesAlbumPreview: View {
+  var album: Album
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      album.pictures[0]
+        .renderingMode(.original)
+        .resizable()
+        .frame(width: 155, height: 155)
+        .cornerRadius(3)
+        .overlay(alignment: .bottomLeading) {
+          Image(systemName: "heart.fill")
+            .foregroundStyle(Color.white)
+            .padding(5)
+        }
+      Text(album.name)
+        .padding(.bottom, -8)
+      Text(String(album.pictureNames.count))
+        .foregroundStyle(Color.gray)
+    }
+  }
+}
+
+#Preview {
+  FavoritesAlbumPreview(album: Album(id: 12, name: "Vacances", pictureNames: ["twinlake", "02", "03", "04", "05", "06", "07", "07","07","07","07","07","07","07","07","07","07","07","07"]))
+}
